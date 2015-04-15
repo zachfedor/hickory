2944e60ed43bd27626ef602f693ea1b07a0be728
zachfedor <zachfedor@gmail.com>
2015-04-15 16:52:27 -0400

    updated base files

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,63 @@
 var gulp = require( 'gulp' ),
-    bower = require( 'gulp-bower' ),
-    sass = require( 'gulp-sass' ),
-    autoprefixer = require( 'gulp-autoprefixer' ),
-    notify = require( 'gulp-notify' ),
-    minifyCSS = require( 'gulp-minify-css' ),
-    rename = require( 'gulp-rename' ),
-    browserSync = require( 'browser-sync' ),
-    reload = browserSync.reload;
+    browserify = require( 'browserify' ),
+    browsersync = require( 'browser-sync' ),
+    source = require( 'vinyl-source-stream' );
+$ = require( 'gulp-load-plugins' )();
 
-var assets = 'wp-content/themes/hickory/assets';
+var assets = './wp-content/themes/hickory/assets';
 var config = {
     sassDir: assets + '/sass',
-    cssDir: assets + '/css',
-    imgDir: assets + '/images',
     jsDir: assets + '/js',
+    imgDir: assets + '/images',
+    fontDir: assets + '/fonts',
+    distDir: assets + '/dist',
     bowerDir: './bower_components'
 }
 
-gulp.task('bower', function() {
-    return bower()
-        .pipe( gulp.dest( config.bowerDir ))
+gulp.task('icons', function() {
+    return gulp.src( config.bowerDir + '/fontawesome/fonts/**.*' )
+        .pipe( gulp.dest( config.fontDir ));
 });
+
+gulp.task('browserify', function() {
+    return browserify( config.jsDir + '/theme.js' )
+        .bundle()
+        .on( 'error', function( err )
+        {
+            console.log( err.toString() );
+            this.emit( 'end' );
+        })
+        .pipe( source( 'theme.bundle.js' ))
+        .pipe( gulp.dest( config.distDir ));
+});
+
+gulp.task('scripts', ['browserify'], function() {
+    return gulp.src( config.distDir + '/theme.bundle.js' )
+//        .pipe( jshint() )
+//        .pipe( jshint.reporter( 'jshint-stylish' ))
+        .pipe( $.rename( 'theme.min.js' ))
+        .pipe( $.uglify() )
+        .pipe( gulp.dest( config.distDir ))
+        .pipe( browsersync.reload({ stream: true, once: true }));
+    });
 
 gulp.task('styles', function() {
     return gulp.src( config.sassDir + '/**/*.scss' )
-        .pipe(sass())
-        .pipe(autoprefixer('last 3 version', 'ie 8', 'ie 9'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(minifyCSS())
-        .pipe(gulp.dest( config.cssDir ))
-        .pipe(reload({stream: true}));
+        .pipe( $.sass() )
+        .pipe( $.autoprefixer( 'last 3 version', 'ie 8', 'ie 9' ))
+        .pipe( $.rename({ suffix: '.min' }))
+        .pipe( $.minifyCss() )
+        .pipe( gulp.dest( config.distDir ))
+        .pipe( browsersync.reload({ stream: true }));
 });
 
 gulp.task('watch', function() {
-    browserSync({
-        proxy: "http://hickory.dev/"
+    browsersync({
+        proxy: "http://localhost/hickory/"
     });
-    gulp.watch(config.sassDir + '/**/*.scss', ['styles']);
-    gulp.watch("./**/*.php").on('change', reload);
+    gulp.watch( config.sassDir + '/**/*.scss', [ 'styles' ]);
+    gulp.watch( config.jsDir + '/**/*.js', [ 'scripts' ]);
+    gulp.watch( "./**/*.php" ).on( 'change', browsersync.reload );
 });
 
-gulp.task('default', ['bower', 'styles']);
+gulp.task( 'default', [ 'scripts', 'styles' ]);
