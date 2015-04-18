b741722a7eb3fb466d55e5989757a6d533f65924
zachfedor <zachfedor@gmail.com>
2015-04-18 00:01:02 -0400

    wrapped wp_nav_menu output in bootstrap classes

--- wp-content/themes/hickory/lib/menu.php
+++ wp-content/themes/hickory/lib/menu.php
@@ -9,7 +9,8 @@ function hickory_do_main_menu()
     $args = array(
         'theme_location'  => 'main_menu',
         'container'       => 'nav',
-        'container_class' => 'main-menu'
+        'container_class' => 'main-menu container',
+        'items_wrap'      => '<ul id="%1$s" class="%2$s row">%3$s</ul>'
     );
 
     wp_nav_menu( $args );
