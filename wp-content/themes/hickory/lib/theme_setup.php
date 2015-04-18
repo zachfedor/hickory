ddf809c27922ec4002fadbf33889772aa1d1e930
zachfedor <zachfedor@gmail.com>
2015-04-18 00:02:51 -0400

    added thumbnail and admin bar support

--- wp-content/themes/hickory/lib/theme_setup.php
+++ wp-content/themes/hickory/lib/theme_setup.php
@@ -4,6 +4,12 @@
  * Theme Setup Template
  */
 
+// Show Admin Toolbar
+show_admin_bar( true );
+
+// Add Thumbnail Support
+add_theme_support( 'post-thumbnails' );
+
 // Register Menus
 register_nav_menus( array(
 	'main_menu' => 'Main Menu',
