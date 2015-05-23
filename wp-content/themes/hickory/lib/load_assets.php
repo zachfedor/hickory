4b28211434c100750ff03cff119af8036959ec7d
zachfedor <zachfedor@gmail.com>
2015-05-23 00:03:05 -0400

    prepping load-assets for javascript

--- wp-content/themes/hickory/lib/load_assets.php
+++ wp-content/themes/hickory/lib/load_assets.php
@@ -4,13 +4,9 @@
  * Load Assets Template
  */
 
-//echo "load assets<br />";
-//echo get_stylesheet_uri() . "<br />";
-error_log("styles prepared");
-
 add_action( 'wp_enqueue_scripts', 'hickory_enqueue_scripts' );
 function hickory_enqueue_scripts() {
 	wp_enqueue_style( 'hickory', get_stylesheet_directory_uri() . '/assets/dist/theme.min.css' );
     error_log("styles enqueued");
-	//wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
+	//wp_enqueue_script( 'hickory-scripts', get_template_directory_uri() . '/assets/dist/them.min.js', array(), '1.0.0', true );
 }
