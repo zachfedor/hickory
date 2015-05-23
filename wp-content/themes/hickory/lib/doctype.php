940aa8581169f37370df6a4b0343fa2ab63d1630
zachfedor <zachfedor@gmail.com>
2015-05-22 22:36:24 -0400

    added google font

--- wp-content/themes/hickory/lib/doctype.php
+++ wp-content/themes/hickory/lib/doctype.php
@@ -12,5 +12,7 @@ function hickory_do_doctype()
 <head>
     <meta name="charset" content="utf-8" />
     <title>Hickory</title>
+
+    <link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
 <?php
 }
