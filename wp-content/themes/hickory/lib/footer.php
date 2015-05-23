3dc774573e7c29a49fa5da27a72428a68c97d13f
zachfedor <zachfedor@gmail.com>
2015-05-23 00:03:21 -0400

    added beginnings of footer

--- wp-content/themes/hickory/lib/footer.php
+++ wp-content/themes/hickory/lib/footer.php
@@ -4,12 +4,20 @@
  * Footer Layout
  */
 
-//echo "footer<br />";
-
 function hickory_do_footer()
 {
     wp_footer();
     ?>
+        <footer>
+            <div class="row">
+                <div class="col-md-6">
+                    &copy; Copyright Hickory Town Workshop <?php echo date('Y'); ?>
+                </div>
+                <div class="col-md-6">
+                    Located in Lancaster, Pennsylvania
+                </div>
+            </div>
+        </footer>
     </body>
     </html>
     <?php
