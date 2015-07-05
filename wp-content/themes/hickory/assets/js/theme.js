34610f3ceb0c5390895fe7788bb8fe01563f56f6
zachfedor <zachfedor@gmail.com>
2015-07-05 07:47:54 -0400

    added javascript to prevent header link from loading

--- wp-content/themes/hickory/assets/js/theme.js
+++ wp-content/themes/hickory/assets/js/theme.js
@@ -7,4 +7,15 @@
  * license: MIT
  */
 
+var $ = require('../../../../../bower_components/jquery/dist/jquery.min.js');
 
+$( document ).ready( function() {
+    $( '#menu-button' ).click( function( event ) {
+        event.preventDefault();
+        $( '#sidebar' ).toggleClass( "active" );
+        /*$( '#menu-button i' ).fadeOut( 400, function() {
+            $( '#menu-button i' ).toggleClass( 'fa-bars' ).toggleClass( 'fa-times' ).fadeIn( 400 );
+        });*/
+        $( '#menu-button i' ).toggleClass( 'fa-bars fa-times' );
+    });
+});
