f104eec818282a4efbcf5bf8f46e7e055be7acf2
zachfedor <zachfedor@gmail.com>
2017-02-01 15:23:16 -0500

    small jquery change

--- wp-content/themes/hickory/assets/js/theme.js
+++ wp-content/themes/hickory/assets/js/theme.js
@@ -13,9 +13,6 @@ $( document ).ready( function() {
     $( '#menu-button' ).click( function( event ) {
         event.preventDefault();
         $( '#sidebar' ).toggleClass( "active" );
-        /*$( '#menu-button i' ).fadeOut( 400, function() {
-            $( '#menu-button i' ).toggleClass( 'fa-bars' ).toggleClass( 'fa-times' ).fadeIn( 400 );
-        });*/
         $( '#menu-button i' ).toggleClass( 'fa-bars fa-times' );
     });
 });
